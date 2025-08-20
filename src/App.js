bcf2c23c93f73fd643f0c68d638237f35d103bc6
Maurya-Anurag14 <233119@theemcoe.org>
2025-08-20 16:20:46 +0530

    Navbar,Sidebar and registration screen is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import React, { useState } from 'react';
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Register from "./components/Register";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('home');
+
+  const handleNavigate = (page) => {
+    setCurrentPage(page);
+  };
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'register':
+        return <Register />;
+      case 'home':
+      default:
+        return <Hero />;
+    }
+  };
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar onNavigate={handleNavigate} />
+      {renderPage()}
     </div>
   );
 }
